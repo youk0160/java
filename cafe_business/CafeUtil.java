ba91e01aee9bbc10d492e87dc4fe3dca0ca10d31
youk0160 <youk0160@gmail.com>
2021-09-28 12:52:16 -0400

    init commit

---
+++ cafe_business/CafeUtil.java
@@ -0,0 +1,44 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class CafeUtil {
+    public static int getStreakGoal(int numWeeks) {
+        int sum=0;
+        for(int i=1; i<=numWeeks; i++) sum+=i;
+        return sum;
+    }
+
+    public static void printPriceChart(String prodName, double price, int maxNum) {
+        double discount=0.0;
+        System.out.println(prodName);
+        for(int i=1; i<=maxNum; i++) {
+            if(i>1) {
+                discount = .5*(i-1);
+            }
+            System.out.printf("%d - $%.2f\n",i,(price*i)-discount);
+        }
+    }
+
+    public static String getOrderTotal(double[] arr) {
+        double sum=0.0;
+        DecimalFormat f = new DecimalFormat("##.00");
+
+        for (double x : arr) {
+            sum += x;
+        }
+        return f.format(sum);
+    }
+
+    public static void displayMenu(ArrayList<String> menu, ArrayList<Double> prices) {
+        for(int i=0; i<menu.size(); i++) {
+            System.out.printf("%s -- $%.2f\n", menu.get(i), prices.get(i));
+        }
+    }
+
+    public static void addCustomer(ArrayList<String> customers) {
+        System.out.print("Please enter your name: ");
+        String userName = System.console().readLine();
+        customers.add(userName);
+        System.out.printf("Hello, %s! There are %d people ahead of you.\n", userName, customers.size()-1);
+    }
+}
---
+++ cafe_business/TestCafe.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class TestCafe {
+    
+	public static void main(String[] args) {
+	    // TO-DO:
+	    // Create an instance of the CafeUtil class        
+	    // in order to use the CafeUtil class' methods.
+    
+	    // Hint: it will need to correspond with the variable name
+	    // used in your test code.
+    
+	    // Given Test Cases Here
+        CafeUtil appTest = new CafeUtil();
+        System.out.println("----- Streak Goal Test -----");
+        System.out.printf("Purchases needed by week 10: %s \n", appTest.getStreakGoal(10));
+
+        System.out.println("----- Price Chart Test-----");
+        appTest.printPriceChart("Chai Latte Mix", 4.5, 3);
+        appTest.printPriceChart("Specialty Coaster", 2.0, 5);
+
+        System.out.println("----- Order Total Test-----");
+        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        System.out.printf("Order total: %s \n",appTest.getOrderTotal(lineItems));
+
+        System.out.println("----- Display Menu Test-----");
+        List<String> loadMenu = Arrays.asList( "drip coffee", "cappucino", "latte", "mocha" );
+        List<Double> loadPrices = Arrays.asList(1.5, 3.5, 4.5, 3.5);
+        ArrayList<String> menu = new ArrayList<String>();
+        ArrayList<Double> prices = new ArrayList<Double>();
+
+        // Note: Below is a way to add multiple elements to a list in fewer lines.
+        // It does require an extra import at the top of the file.
+        menu.addAll(loadMenu);
+        prices.addAll(loadPrices);
+        appTest.displayMenu(menu, prices);
+
+        System.out.println("----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        // Test 4 times
+        for (int i = 0; i < 4; i++) { 
+            appTest.addCustomer(customers); 
+            System.out.println(customers);
+        }
+    }
+
+}
