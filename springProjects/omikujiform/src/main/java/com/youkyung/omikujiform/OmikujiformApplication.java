5e8f9a5dc178a30ca900112ff1780236845c0076
You K. Lee <32504030+youk0160@users.noreply.github.com>
2021-10-08 09:27:22 -0400

    init commit

---
+++ springProjects/omikujiform/src/main/java/com/youkyung/omikujiform/OmikujiformApplication.java
@@ -0,0 +1,13 @@
+package com.youkyung.omikujiform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmikujiformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmikujiformApplication.class, args);
+	}
+
+}
